35f54ba47b7a0d7cbff2e5be7efdcb4ac5cda06b
chamodnilushanka21-code <chamodnilushanka21@gmail.com>
2025-12-24 17:57:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,102 +1,27 @@
-import os
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.clock import Clock
-import speech_recognition as sr
-from twilio.rest import Client
-import random
+name: Final Build
+on:
+  workflow_dispatch:
+  push:
+    branches: [ main ]
 
-# --- මෙතනට ඔයාගේ Twilio විස්තර දාන්න ---
-account_sid = 'AC8b73786b6d379fba78d89afd2dbc1205' # ඔයාගේ Account SID එක
-auth_token = '62cd316b6d38a17b7986414192fe80d3'   # ඔයාගේ Auth Token එක
-twilio_number = '+1 802 802 9558'                    # ඔයාගේ Twilio Number එක
-# ----------------------------------------
-
-KV = '''
-ScreenManager:
-    LanguageScreen:
-    RegScreen:
-    OTPScreen:
-    MainApp:
-
-<LanguageScreen>:
-    name: 'lang_screen'
-    BoxLayout:
-        orientation: 'vertical'
-        Button:
-            text: 'සිංහල'
-            on_release: root.set_lang('si')
-        Button:
-            text: 'English'
-            on_release: root.set_lang('en')
-
-<RegScreen>:
-    name: 'reg_screen'
-    BoxLayout:
-        orientation: 'vertical'
-        TextInput:
-            id: phone_input
-            hint_text: 'Phone Number (+947...)'
-        Button:
-            text: 'Get OTP'
-            on_release: root.send_otp()
-
-<OTPScreen>:
-    name: 'otp_screen'
-    BoxLayout:
-        orientation: 'vertical'
-        TextInput:
-            id: otp_input
-            hint_text: 'Enter OTP'
-        Button:
-            text: 'Verify'
-            on_release: root.verify_otp()
-
-<MainApp>:
-    name: 'main_app'
-    BoxLayout:
-        orientation: 'vertical'
-        Label:
-            id: lb
-            text: 'Listening...'
-        Button:
-            text: 'Speak'
-            on_release: root.process()
-'''
-
-class LanguageScreen(Screen):
-    def set_lang(self, lang):
-        App.get_running_app().lang = lang
-        self.manager.current = 'reg_screen'
-
-class RegScreen(Screen):
-    def send_otp(self):
-        phone = self.ids.phone_input.text
-        otp = str(random.randint(1000, 9999))
-        App.get_running_app().otp = otp
-        try:
-            client = Client(account_sid, auth_token)
-            client.messages.create(body=f'Your OTP is {otp}', from_=twilio_number, to=phone)
-            self.manager.current = 'otp_screen'
-        except:
-            print("Error sending SMS")
-
-class OTPScreen(Screen):
-    def verify_otp(self):
-        if self.ids.otp_input.text == App.get_running_app().otp:
-            self.manager.current = 'main_app'
-
-class MainApp(Screen):
-    def process(self):
-        # Voice Command Logic Here
-        self.ids.lb.text = "Call logic goes here"
-
-class VoiceProApp(App):
-    lang = 'si'
-    otp = ''
-    def build(self):
-        return Builder.load_string(KV)
-
-if _name_ == '_main_':
-    VoiceProApp().run()
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - name: Set up Python
+        uses: actions/setup-python@v2
+        with:
+          python-version: '3.9'
+      - name: Install Buildozer
+        run: |
+          sudo apt update
+          sudo apt install -y git zip unzip autoconf libtool pkg-config zlib1g-dev libncurses5-dev libncursesw5-dev libtinfo5 cmake libffi-dev libssl-dev
+          pip install --user --upgrade buildozer cython virtualenv
+      - name: Build with Buildozer
+        run: buildozer android debug
+      - name: Upload Artifact
+        uses: actions/upload-artifact@v2
+        with:
+          name: VoicePro-Final-Release
+          path: bin/*.apk
